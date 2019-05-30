e020f71964e8fcb06f73add50441003b5ddaebbe
Darryl Kilzer <darrylkilzer@gmail.com>
2019-05-30 11:18:31 -0600

    Danielle

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -11,42 +11,48 @@ export default class UserController {
   constructor() {
     this.router = express.Router()
       .get('', this.getAllUsers)
+      .get('/:name', this.getUserByName)
+      .put('/:id', this.editUser)
+      .post('', this.createUser)
+      .delete('/:id', this.deleteUser)
+      .use('*', this.defaultRoute)
   }
 
   async getAllUsers(req,res,next) {
     try {
-      
-    } catch (error) {
-      
-    }
+      let users = await _repo.find({})
+      return res.send(users)
+    } catch (error) {next(error)}
   }
-  async getUserById(req,res,next) {
+  async getUserByName(req,res,next) {
     try {
-      
-    } catch (error) {
-      
-    }
+      let user = await _repo.findOne({name: req.params.name})
+    } catch (error) {next(error)}
   }
   async editUser(req,res,next) {
     try {
-      
+      let users = await _repo
     } catch (error) {
       
     }
   }
   async createUser(req,res,next) {
     try {
-      
+      let users = await _repo
     } catch (error) {
       
     }
   }
   async deleteUser(req,res,next) {
     try {
-      
+      let users = await _repo
     } catch (error) {
       
     }
+  }
+
+  defaultRoute(req, res, next){
+    next({status: 400, message: 'no such user'})
   }
 
 }
