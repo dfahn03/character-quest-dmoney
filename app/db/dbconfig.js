f6050e8f3b3ecb508b09757f9659cdd6597400f2
Darryl Kilzer <darrylkilzer@gmail.com>
2019-05-30 10:56:52 -0600

    david

---
+++ app/db/dbconfig.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const connectionString = 'mongodb+srv://student:student@cluster0-wtsis.mongodb.net/characterquest?retryWrites=true&w=majority'
+let connection = mongoose.connection
+mongoose.connect(connectionString,{
+  useNewUrlParser: true
+})
+connection.on('error',err=>{
+  console.error('[DATABASE ERROR]:',err)
+})
+connection.once('open',()=>{
+  console.log('connected to the database')
+})
